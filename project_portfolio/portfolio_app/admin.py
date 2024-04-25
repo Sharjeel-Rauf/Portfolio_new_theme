a811ca4cbfc8a34aae1b9e692735c638550d2fa1
Sharjeel Rauf <sharjeelrauf119202@gmail.com>
2024-04-26 03:54:01 +0500

    made changes in the offline pages tabs and edited the buttons in the project details page

--- project_portfolio/portfolio_app/admin.py
+++ project_portfolio/portfolio_app/admin.py
@@ -8,3 +8,9 @@ admin.site.register(ProjectModel)
 admin.site.register(ProjectDetailModel)
 admin.site.register(Summary)
 admin.site.register(UserProfile)
+admin.site.register(ProfileDescription)
+admin.site.register(WorkExperience)
+admin.site.register(Education)
+admin.site.register(Discipline)
+
+
